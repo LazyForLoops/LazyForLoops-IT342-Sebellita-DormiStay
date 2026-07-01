48b41de541bc4e3f96fa16ded31bec90684c1eeb
LAB22_USER ON WS18 <L22X16W18@LabHCI.net>
2026-07-01 16:17:40 +0800

    initial springboot

---
+++ backend/dormistay/src/main/java/edu/cit/sebellita/dormistay/DormistayApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.sebellita.dormistay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormistayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormistayApplication.class, args);
+	}
+
+}
---
+++ backend/dormistay/src/test/java/edu/cit/sebellita/dormistay/DormistayApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.sebellita.dormistay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormistayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
